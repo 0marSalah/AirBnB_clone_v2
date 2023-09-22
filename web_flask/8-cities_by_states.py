b82d03eb9a0ed1c3f9655dcaefdc19640e7f5eba
0marSalah <omar.salah1597@gmail.com>
2023-09-22 22:33:48 +0300

    doc display

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
+  """ display a HTML page with the states listed in alphabetical order"""
   states =storage.all('State').values()
   return render_template('8-cities_by_states.html', states=states)
 
